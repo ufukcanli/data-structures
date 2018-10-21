122e68dd3daa26e52221a01d03d7b393510904be
ufuk <iletisim@ufukcanli.com>
2018-10-21 20:16:41 +0300

    made some error just a second ago but now added the remove function.

--- singly_linked_list.c
+++ singly_linked_list.c
@@ -18,11 +18,11 @@ struct List
 
 // function prototypes
 void init(struct List *list_ptr);
-void menu();
 void prepend(struct List *list_ptr, int value);
 void append(struct List *list_ptr, int value);
+void remove_by_value(struct List *list_ptr, int value);
 void print_list(struct List *list_ptr);
-struct Node *find_by_value(struct List *list_ptr, int value);
+void menu();
 
 int main()
 {
@@ -57,6 +57,11 @@ int main()
       printf("Here is the linked list so far: ");
       print_list(&linked_list);
       break;
+    case 4:
+      printf("Enter a value from the list to remove the node: ");
+      scanf("%d", &number);
+      remove_by_value(&linked_list, number);
+      break;
     default:
       printf("Please enter a valid option :(\n");
     }
@@ -71,17 +76,6 @@ void init(struct List *list_ptr)
   list_ptr->head = NULL;
   list_ptr->tail = NULL;
   list_ptr->node_counter = 0;
-}
-
-// starting point
-void menu()
-{
-  printf("Singly Linked List Operations\n");
-  printf("-----------------------------\n");
-  printf("1) Add a new node at the beginning of the list\n");
-  printf("2) Add a new node at the end of the list\n");
-  printf("3) Print the list from the beginning to the end\n");
-  printf("0) Quit the program\n");
 }
 
 // add a new node at the beginning of the linked list
@@ -154,19 +148,37 @@ void print_list(struct List *list_ptr)
   printf("\n");
 }
 
-struct Node *find_by_value(struct List *list_ptr, int value)
+void remove_by_value(struct List *list_ptr, int value)
 {
-  struct Node *iter = list_ptr->head;
+  struct Node *temp = list_ptr->head;
 
-  while (iter != NULL)
+  if (list_ptr->head->data == value)
   {
-    if (iter->data == value)
-    {
-      break;
-    }
-
-    iter = iter->next;
+    list_ptr->head = list_ptr->head->next;
+    free(temp);
   }
 
-  return iter;
+  while (temp->next->data != value)
+  {
+    temp = temp->next;
+  }
+
+  struct Node *deleted = temp->next;
+  temp->next = deleted->next;
+
+  free(deleted);
+
+  list_ptr->node_counter--;
+}
+
+// starting point
+void menu()
+{
+  printf("Singly Linked List Operations\n");
+  printf("-----------------------------\n");
+  printf("1) Add a new node at the beginning of the list\n");
+  printf("2) Add a new node at the end of the list\n");
+  printf("3) Print the list from the beginning to the end\n");
+  printf("4) Remove a node with a given value\n");
+  printf("0) Quit the program\n");
 }
