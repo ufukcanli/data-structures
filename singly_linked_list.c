b0c5691b0ff381162cf22e2457f454bcd596ce9b
ufukcanli <iletisim@ufukcanli.com>
2019-12-28 20:33:54 +0300

    new implementation of the singly linked list added

--- singly_linked_list.c
+++ singly_linked_list.c
@@ -1,189 +1,289 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// structure of node
 struct Node
 {
-  int data;
-  struct Node *next;
+    int data;
+    struct Node *next;
 };
 
-// will make it shorter the code
 typedef struct Node node;
 
-// head instance of the list
-node *head;
+node *create_list(node *head, int value);
+node *add_first(node *head, int value);
+node *add_last(node *head, int value);
+node *add_before(node *head, int value, int before);
+node *add_after(node *head, int value, int after);
+node *remove_first(node *head);
+node *remove_last(node *head);
+node *remove_node(node *head, int value);
+node *destroy_list(node *head);
+node *sort_list(node *head);
+void display_list(node *head);
+void display_list_reverse(node *head);
+int length(node *head);
+int length_rec(node *head);
 
-// function prototypes
-void init();
-void prepend(int val);
-void append(int val);
-void remove_by_val(int val);
-void remove_by_pos(int pos);
-void insert(int pos, int val);
-void display();
-void count();
-
-int main()
+void main()
 {
-  init();
+    node *head = create_list(head, 0);
 
-  display();
+    head = add_first(head, 5);
 
-  append(1);
-  prepend(0);
-  append(2);
-  prepend(-1);
+    head = add_last(head, 6);
 
-  display();
+    head = add_before(head, 10, 6);
 
-  count();
+    head = add_after(head, 11, 10);
 
-  remove_by_val(2);
-  display();
+    head = add_before(head, 12, 11);
 
-  insert(2, 4);
-  display();
+    head = add_after(head, 13, 12);
 
-  count();
+    display_list(head);
 
-  return 0;
+    // head = remove_first(head);
+
+    // display_list(head);
+
+    // head = remove_last(head);
+
+    // display_list(head);
+
+    // head = remove_node(head, 12);
+
+    // display_list(head);
+
+    // head = destroy_list(head);
+
+    // display_list(head);
+
+    // display_list_reverse(head);
+
+    head = sort_list(head);
+
+    display_list(head);
+
+    printf("Length of the list: %d\n", length(head));
+    printf("Length of the list: %d\n", length_rec(head));
 }
 
-// it is obvious
-void init()
+node *create_list(node *head, int value)
 {
-  head = NULL;
+    if (head != NULL)
+    {
+        node *new_node = malloc(sizeof(node));
+
+        new_node->data = value;
+        new_node->next = NULL;
+
+        head = new_node;
+    }
+
+    return head;
 }
 
-// add a new node at the beginning of the list
-void prepend(int val)
+void display_list(node *head)
 {
-  node *new = malloc(sizeof(node));
+    while (head != NULL)
+    {
+        printf("%d => ", head->data);
+        head = head->next;
+    }
 
-  new->data = val;
-  new->next = head;
-
-  head = new;
+    printf("\n");
 }
 
-// add a new node at the end of the list
-void append(int val)
+node *add_first(node *head, int value)
 {
-  node *new = malloc(sizeof(node));
+    node *new_node = malloc(sizeof(node));
 
-  new->data = val;
-  new->next = NULL;
+    new_node->data = value;
+    new_node->next = head;
 
-  if (head == NULL)
-  {
-    head = new;
-    return;
-  }
+    head = new_node;
 
-  node *iter = head;
-
-  while (iter->next != NULL)
-  {
-    iter = iter->next;
-  }
-
-  iter->next = new;
+    return head;
 }
 
-// removes node with a given value
-void remove_by_val(int val)
+node *add_last(node *head, int value)
 {
-  node *temp = head;
+    node *new_node = malloc(sizeof(node));
 
-  if (head->data == val)
-  {
+    new_node->data = value;
+    new_node->next = NULL;
+
+    node *ptr = head;
+
+    while (ptr->next != NULL)
+        ptr = ptr->next;
+
+    ptr->next = new_node;
+
+    return head;
+}
+
+node *add_before(node *head, int value, int before)
+{
+    node *new_node, *prev_node, *ptr;
+
+    new_node = malloc(sizeof(node));
+    new_node->data = value;
+
+    ptr = head;
+
+    while (ptr->data != before)
+    {
+        prev_node = ptr;
+        ptr = ptr->next;
+    }
+
+    prev_node->next = new_node;
+    new_node->next = ptr;
+
+    return head;
+}
+
+node *add_after(node *head, int value, int after)
+{
+    node *new_node, *prev_node, *ptr;
+
+    new_node = malloc(sizeof(node));
+    new_node->data = value;
+
+    ptr = head;
+    prev_node = ptr;
+
+    while (prev_node->data != after)
+    {
+        prev_node = ptr;
+        ptr = ptr->next;
+    }
+
+    prev_node->next = new_node;
+    new_node->next = ptr;
+
+    return head;
+}
+
+node *remove_first(node *head)
+{
+    node *temp = head;
+
     head = head->next;
+
     free(temp);
-  }
 
-  while (temp->next->data != val)
-  {
-    temp = temp->next;
-  }
-
-  node *del = temp->next;
-  temp->next = del->next;
-  free(del);
+    return head;
 }
 
-// removes node with a given position
-void remove_by_pos(int pos)
+node *remove_last(node *head)
 {
-  node *iter = head;
+    node *temp, *ptr;
 
-  for (int i = 0; i < pos - 1; i++)
-  {
-    iter = iter->next;
-  }
+    ptr = head;
 
-  node *temp = iter->next;
+    while (ptr->next->next != NULL)
+        ptr = ptr->next;
 
-  iter->next = iter->next->next;
+    temp = ptr->next->next;
 
-  free(temp);
+    free(temp);
+
+    ptr->next = NULL;
+
+    return head;
 }
 
-// inserts node at a specific position
-void insert(int pos, int val)
+node *remove_node(node *head, int value)
 {
-  node *new = malloc(sizeof(node));
+    node *temp = head;
 
-  new->data = val;
+    if (temp->data == value)
+    {
+        head = remove_first(head);
+        return head;
+    }
 
-  if (pos == 1)
-  {
-    prepend(val);
-    return;
-  }
+    node *ptr;
 
-  node *iter = head;
+    while (temp->data != value)
+    {
+        ptr = temp;
+        temp = temp->next;
+    }
 
-  for (int i = 0; i < pos - 1; i++)
-  {
-    iter = iter->next;
-  }
+    ptr->next = temp->next;
 
-  new->next = iter->next;
-  iter->next = new;
+    free(temp);
+
+    return head;
 }
 
-// print the list to the console
-void display()
+node *destroy_list(node *head)
 {
-  if (head == NULL)
-  {
-    printf("EMPTY!");
-  }
+    node *ptr = head;
 
-  node *iter = head;
+    while (ptr != NULL)
+    {
+        head = remove_first(head);
+        ptr = head;
+    }
 
-  while (iter != NULL)
-  {
-    printf("%d => ", iter->data);
-    iter = iter->next;
-  }
-
-  printf("\n");
+    return head;
 }
 
-// counts nodes in the list
-void count()
+void display_list_reverse(node *head)
 {
-  int counter = 0;
+    if (head == NULL)
+        return;
 
-  node *iter = head;
+    display_list_reverse(head->next);
+    printf("%d => ", head->data);
+}
 
-  while (iter != NULL)
-  {
-    counter++;
-    iter = iter->next;
-  }
+node *sort_list(node *head)
+{
+    node *ptr1, *ptr2;
+    int temp;
 
-  printf("Length of the list: %d\n", counter);
+    ptr1 = head;
+    while (ptr1->next != NULL)
+    {
+        ptr2 = ptr1->next;
+        while (ptr2 != NULL)
+        {
+            if (ptr1->data > ptr2->data)
+            {
+                temp = ptr1->data;
+                ptr1->data = ptr2->data;
+                ptr2->data = temp;
+            }
+            ptr2 = ptr2->next;
+        }
+        ptr1 = ptr1->next;
+    }
+
+    return head;
+}
+
+int length(node *head)
+{
+    int counter = 0;
+
+    while (head != NULL)
+    {
+        counter++;
+        head = head->next;
+    }
+
+    return counter;
+}
+
+int length_rec(node *head)
+{
+    if (head == NULL)
+        return 0;
+
+    return 1 + length(head->next);
 }
